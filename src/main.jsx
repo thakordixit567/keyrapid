5c2398376a3f5118b4374bfd9bf72acc6c2aa8d1
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-09-24 12:01:21 +0530

    UPDATE!

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,24 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home/HomePage.jsx";
+import TyperPage from "./pages/Typer/TyperPage.jsx";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <HomePage/>,
+      },
+      {
+        path: "/typing-speed-test",
+        element: <TyperPage/>
+      }
+    ]
   },
 ]);
 
---
+++ src/pages/Typer/TyperPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TyperPage = () => {
+  return (
+    <div>TyperPage</div>
+  )
+}
+
+export default TyperPage
