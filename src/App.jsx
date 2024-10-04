5ba8159f2944bd3a1af32daaf964bf1e1ad1b84d
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-10-04 13:07:21 +0530

    UPDATE!

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,21 @@ import "./App.css";
 import Header from "./component/Header";
 import FooterPro from "./component/FooterPro";
 import { getRandomText } from "./utils/randomText";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
-   const text = getRandomText();
-   console.log(text)
-
+  const dispatch = useDispatch();
+  const { text } = useSelector((state) => state.typingTest);
+  console.log(text);
   return (
     <>
-     <div className=" max-w-screen-2xl mx-auto md:px-4">
-     <Header/>
-      <div className=" min-h-[ calc(100vh - 80px)]">
-      <Outlet />
+      <div className=" max-w-screen-2xl mx-auto md:px-4">
+        <Header />
+        <div className=" min-h-[ calc(100vh - 80px)]">
+          <Outlet />
+        </div>
+        <FooterPro />
       </div>
-     <FooterPro/>
-     </div>
     </>
   );
 }
